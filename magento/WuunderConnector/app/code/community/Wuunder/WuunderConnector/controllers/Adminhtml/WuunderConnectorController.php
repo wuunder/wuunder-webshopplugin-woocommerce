4408b7403d0510257e193bdc72f13306c40a13d3
riklempens <rik@transceptor.technology>
2017-05-09 14:21:34 +0200

    Removed magento code as this is now in a separate repository.

--- magento/WuunderConnector/app/code/community/Wuunder/WuunderConnector/controllers/Adminhtml/WuunderConnectorController.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Wuunder_WuunderConnector_Adminhtml_WuunderConnectorController extends Mage_Adminhtml_Controller_Action
-{
-    protected function _isAllowed()
-    {
-        return Mage::getSingleton('admin/session')->isAllowed('system/config/wuunderconnector');
-    }
-}
