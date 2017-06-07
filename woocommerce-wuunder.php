9322e010c5eef437106cefb64cdc19838dd0f6c3
riklempens <rik@transceptor.technology>
2017-06-07 09:31:49 +0200

    Upped version to 1.0

--- woocommerce-wuunder.php
+++ woocommerce-wuunder.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce Wuunder
  * Plugin URI: http://wearewuunder.com
  * Description: Wuunder shipping plugin
- * Version: 0.9.1
+ * Version: 1.0
  * Author: Wuunder
  * Author URI: http://wearewuunder.com
  */
@@ -30,7 +30,7 @@ if( !class_exists('Woocommerce_Wuunder') ) {
 		public static $plugin_path;
 		public static $plugin_basename;
 
-		const VERSION = '0.9.1';
+		const VERSION = '1.0';
 
 		public function __construct() {
 
