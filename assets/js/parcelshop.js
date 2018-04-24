c9a1cd013dddb0d8d184a31969d87c79ae058d92
kFrolichs <k.frolichs@gmail.com>
2018-04-24 15:53:56 +0200

    First work on parcelshop implementation

--- assets/js/parcelshop.js
+++ assets/js/parcelshop.js
@@ -1,6 +1,21 @@
+// Get the modal
+var modal = document.getElementById('myModal');
 
-alert("Dit is een test die ik doe");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 function showParcelshopPicker(){
-  console.log("Hoi");
+    modal.style.display = "block";
 }
--- includes/checkout.php
+++ includes/checkout.php
@@ -1,17 +1,40 @@
 <?php
 if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
+add_action('wp_enqueue_scripts', 'callback_for_setting_up_scripts');
 add_action('woocommerce_before_order_notes', 'parcelshop_html');
 
+function callback_for_setting_up_scripts() {
+    $pluginPath = dirname(plugin_dir_url(__FILE__));
+    $pluginPath .= "/assets/css/parcelshop.css";
+    wp_register_style( 'wuunderCSS', $pluginPath);
+    wp_enqueue_style( 'wuunderCSS' );
+}
+
 function parcelshop_html(){
-  $pluginPath = plugins_url();
-  $pluginPath .= "/woocommerce-wuunder/assets/js/parcelshop.js";
-  echo <<<EOT
-  <script type="text/javascript">
-  console.log("Dit is het path: ");
-  console.log("$pluginPath");
-  </script>
-  <script type="text/javascript" data-cfasync="false" src="$pluginPath"></script>
+    $pluginPath = dirname(plugin_dir_url(__FILE__));
+    $pluginPathJS = $pluginPath . "/assets/js/parcelshop.js";
+    $pluginPathImg = $pluginPath . "/assets/images/parcelshop/bring-to-parcelshop.png";
+    echo <<<EOT
+    <div id="myModal" class="modal">
+      <div class="modal-content">
+
+        <div>
+          <img id=bring-to-parcelshop src="$pluginPathImg" alt="parcelshop">
+          <span id=parcelShopsTitleLogoChatbox>Kies een parcelshop</span>
+          <span class="close">&times;</span>
+        </div>
+
+        <td>
+          <span id="parcelShopsSearchBarContainer">
+          <input id="parcelShopsSearchBar" type="text" placeholder="Search for address">
+          <span id="submitParcelShopsSearchBar">OK</span>
+          </span>
+        </td>
+
+      </div>
+    </div>
+    <script type="text/javascript" data-cfasync="false" src="$pluginPathJS"></script>
 EOT;
 echo '<div id="parcelshopsSelectedContainer" onclick="showParcelshopPicker()"><a href="#" id="selectParcelshop">Klik hier om een parcelshop te kiezen</a></div>';
 }
