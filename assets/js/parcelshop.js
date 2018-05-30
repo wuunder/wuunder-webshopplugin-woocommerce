fc037a944851f9bdd555f5eb104a1c320da98279
kFrolichs <k.frolichs@gmail.com>
2018-05-30 16:07:34 +0200

    Removed unnecesary logging

--- assets/js/parcelshop.js
+++ assets/js/parcelshop.js
@@ -241,7 +241,6 @@ function ajaxRequest(address = null) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        console.log(xhttp.responseText);
           var val = JSON.parse(xhttp.responseText.substring(0, xhttp.responseText.length - 1));
           displayMap(val.location);
           setAddress(val.address);
