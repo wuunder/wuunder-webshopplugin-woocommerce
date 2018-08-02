79475279b4f9d7de4fef1abd54f10427e6b25d02
Timo Janssrn <tj.timo.j@gmail.com>
2018-08-02 08:39:35 +0200

    .

--- woocommerce-wuunder.php
+++ woocommerce-wuunder.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce Wuunder
  * Plugin URI: http://wearewuunder.com
  * Description: Wuunder shipping plugin
- * Version: 2.4.1
+ * Version: 2.4.2
  * Author: Wuunder
  * Author URI: http://wearewuunder.com
  */
@@ -51,7 +51,7 @@ if (!class_exists('Woocommerce_Wuunder')) {
         public static $plugin_path;
         public static $plugin_basename;
 
-        const VERSION = '2.4.1';
+        const VERSION = '2.4.2';
 
         public function __construct()
         {
