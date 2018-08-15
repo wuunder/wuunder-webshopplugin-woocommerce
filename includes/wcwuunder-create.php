7704efdad00c69774b1aa9ee918b92c3c5927bd9
Timo Janssrn <tj.timo.j@gmail.com>
2018-08-15 13:17:14 +0200

    versioning

--- includes/wcwuunder-create.php
+++ includes/wcwuunder-create.php
@@ -13,7 +13,7 @@ if (!class_exists('WC_Wuunder_Create')) {
             $this->version_obj = array(
                 "product" => "Woocommerce extension",
                 "version" => array(
-                    "build" => "2.4.3",
+                    "build" => "2.4.4",
                     "plugin" => "2.0"),
                 "platform" => array(
                     "name" => "Woocommerce",
