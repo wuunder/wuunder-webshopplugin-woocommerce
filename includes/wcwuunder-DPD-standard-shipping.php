03ddecd56d2ca7eee8dc1f234b8459ebb41fd355
Tim Dohmen <tim.dohmen@wearewuunder.com>
2019-02-28 14:57:46 +0100

    Name changes on admin page, not on checkout page

--- includes/wcwuunder-DPD-standard-shipping.php
+++ includes/wcwuunder-DPD-standard-shipping.php
@@ -28,35 +28,6 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
                         'instance-settings',
                         'instance-settings-modal'
                     );
-
-                    // These are the options set by the user
-                    $this->cost = $this->get_option( 'cost' );
-                    $this->carriers = $this->get_option( 'select_carriers');
-                    $this->instance_form_fields = array(
-                        'title' => array(
-                            'title'         => __( 'Naam van de verzendmethode' ),
-                            'type'          => 'text',
-                            'description'   => __( 'Dit stelt de naam van de verzendmethode in op de check-out pagina.' ),
-                            'default'       => __( 'Aflevering op huisadres via DPD' ),
-                            'desc_tip'      => true
-                        ),
-                        'cost'      => array(
-                            'title'         => __( 'Kosten', 'woocommerce' ),
-                            'type'          => 'number',
-                            'description'   => __( 'Kosten voor gebruik van de verzendmethode', 'woocommerce' ),
-                            'default'       => 3.50,
-                            'desc_tip'      => true
-                        ),
-                        'free_from' => array(
-                            'title'         => __( 'Gratis verzending vanaf', 'woocommerce' ),
-                            'type'          => 'number',
-                            'description'   => __( 'Vanaf welk bestelbedrag is de verzending gratis. Stel 0 in voor nooit.', 'woocommerce' ),
-                            'default'       => 50.00,
-                            'desc_tip'      => true
-                        )
-                    );
-                    $this->title = $this->get_option( 'title' );
-
                     $this->init();
                 }
 
@@ -70,8 +41,41 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
                     // Load the settings API
                     $this->init_form_fields(); // This is part of the settings API. Override the method to add your own settings
                     $this->init_settings(); // This is part of the settings API. Loads settings you previously init.
+
+                    // These are the options set by the user
+                    $this->cost = $this->get_option( 'cost' );
+                    $this->carriers = $this->get_option( 'select_carriers');
+                    $this->title = $this->get_option( 'title' );
                     // Save settings in admin if you have any defined
                     add_action( 'woocommerce_update_options_shipping_' . $this->id, array( $this, 'process_admin_options' ) );
+                }
+
+                function init_form_fields() {
+
+                    $this->instance_form_fields = array(
+                        'title' => array(
+                            'title'         => __( 'Naam van de verzendmethode', 'woocommerce' ),
+                            'type'          => 'text',
+                            'description'   => __( 'Dit stelt de naam van de verzendmethode in op de check-out pagina.', 'woocommerce' ),
+                            'default'       => __( 'Aflevering op huisadres via DPD' ),
+                            'desc_tip'      => true
+                        ),
+                        'cost'      => array(
+                            'title'         => __( 'Kosten', 'woocommerce' ),
+                            'type'          => 'number',
+                            'description'   => __( 'Kosten voor gebruik van de verzendmethode', 'woocommerce', 'woocommerce' ),
+                            'default'       => 3.50,
+                            'desc_tip'      => true
+                        ),
+                        'free_from' => array(
+                            'title'         => __( 'Gratis verzending vanaf', 'woocommerce' ),
+                            'type'          => 'number',
+                            'description'   => __( 'Vanaf welk bestelbedrag is de verzending gratis. Stel 0 in voor nooit.', 'woocommerce' ),
+                            'default'       => 50.00,
+                            'desc_tip'      => true
+                        ),
+                    );
+
                 }
 
                 /**
